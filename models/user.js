48a877756d0032a872bc66b3cdeddfdc2e3e954c
Hristo Raykov <hristocr@gmail.com>
2018-07-16 19:35:21 +0300

    Loading skeleton

--- models/user.js
+++ models/user.js
@@ -1,28 +1,38 @@
 const Sequelize = require('sequelize');
+const encryption = require("../utilities/encryption");
 
-module.exports = function (sequilize,DataType) {
-	const User = sequilize.define('User', {
-		email: {
-			type: Sequelize.STRING,
-			required: true,
-			uniqe: true,
-			allowNull: false
-		},
-		passwordHash: {
-			type: Sequelize.STRING,
-			required: true
-		},
-		fullName: {
-			type: Sequelize.STRING,
-			required: true
-		},
-		salt: {
-			type: Sequelize.STRING,
-			required: true
-		},
-	}, {
-		timestamps: false
-	});
-	
-	return User;
+module.exports = function (sequelize) {
+    const User = sequelize.define('User', {
+        email: {
+            type: Sequelize.STRING,
+            required: true,
+            unique: true,
+            allowNull: false
+        },
+        passwordHash: {
+            type: Sequelize.STRING,
+            required: true
+        },
+        fullName: {
+            type: Sequelize.STRING,
+            required: true
+        },
+        salt: {
+            type: Sequelize.STRING,
+            required: true
+        },
+
+    }, {
+        timestamps: false
+    });
+
+    User.prototype.authenticate = function (password) {
+        let inputPasswordHash = encryption.hashPassword(password, this.salt);
+        return inputPasswordHash === this.passwordHash;
+    };
+
+
+    return User;
+
+
 };
