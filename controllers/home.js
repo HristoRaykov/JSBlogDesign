a802d0dea091b95bbe30044e597c75977f95ad2c
Hristo Raykov <hristocr@gmail.com>
2018-07-17 21:45:23 +0300

    adding article fuctionality

--- controllers/home.js
+++ controllers/home.js
@@ -1,9 +1,18 @@
+const Article = require('../models').Article;
+const User = require('../models').User;
+
 module.exports = {
-    index: (req, res) => {
-
-        res.render('home/index');
-
-    }
+	index: (req, res) => {
+		Article
+			.findAll({
+				limit: 6, include: [{
+					model: User
+				}]
+			})
+			.then(articles => {
+				res.render('home/index', {articles: articles});
+			});
+	}
 };
 
 
